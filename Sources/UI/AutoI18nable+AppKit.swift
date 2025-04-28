a60ee43d7a21fd460e7644ae15608890e060dbe1
CodeEagle <stasura@hotmail.com>
2025-04-28 09:46:31 +0800

    refactor(UI): 移除setString函数默认tag参数

--- Sources/UI/AutoI18nable+AppKit.swift
+++ Sources/UI/AutoI18nable+AppKit.swift
@@ -9,7 +9,18 @@ import Foundation
 
 #if os(macOS)
     import AppKit
-
+    extension NSTableColumn: AutoI18nable {
+        public func setString(value: String, forTag tag: String? = nil) {
+            switch tag {
+            case "headerToolTip":
+                headerToolTip = value
+                break
+            default:
+                title = value
+                break
+            }
+        }
+    }
     extension NSSegmentedControl: AutoI18nable {
         public func setAutoLize(label: String, forSegment segment: Int) {
             autoLocalize(key: label, tag: "\(segment)")
@@ -25,14 +36,13 @@ import Foundation
     // MARK: - NSTextField AutoI18nable implementation
 
     extension NSTextField: AutoI18nable {
-        public func setString(value: String, forTag tag: String? = "text") {
+        public func setString(value: String, forTag tag: String? = nil) {
             switch tag {
-            case "text":
-                stringValue = value
-                break
             case "placeholder":
                 placeholderString = value
+                break
             default:
+                stringValue = value
                 break
             }
         }
@@ -42,13 +52,13 @@ import Foundation
 
     extension NSButton: AutoI18nable {
 
-        public func setString(value: String, forTag tag: String? = "title") {
+        public func setString(value: String, forTag tag: String? = nil) {
             switch tag {
-            case "title":
-                title = value
             case "tooltip":
                 toolTip = value
+                break
             default:
+                title = value
                 break
             }
         }
@@ -57,11 +67,10 @@ import Foundation
     // MARK: - NSTextView AutoI18nable implementation
 
     extension NSTextView: AutoI18nable {
-        public func setString(value: String, forTag tag: String? = "string") {
+        public func setString(value: String, forTag tag: String? = nil) {
             switch tag {
-            case "string":
-                string = value
             default:
+                string = value
                 break
             }
         }
@@ -70,13 +79,13 @@ import Foundation
     // MARK: - NSMenuItem AutoI18nable implementation
 
     extension NSMenuItem: AutoI18nable {
-        public func setString(value: String, forTag tag: String? = "title") {
+        public func setString(value: String, forTag tag: String? = nil) {
             switch tag {
-            case "title":
-                title = value
             case "tooltip":
                 toolTip = value
+                break
             default:
+                title = value
                 break
             }
         }
@@ -85,13 +94,13 @@ import Foundation
     // MARK: - NSTabViewItem AutoI18nable implementation
 
     extension NSTabViewItem: AutoI18nable {
-        public func setString(value: String, forTag tag: String? = "label") {
+        public func setString(value: String, forTag tag: String? = nil) {
             switch tag {
-            case "label":
-                label = value
             case "tooltip":
                 toolTip = value
+                break
             default:
+                label = value
                 break
             }
         }
